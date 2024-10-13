ad1f5be12cc7598c231d74c587b96e7ff53470b7
tarungottipalli <tarun1219@sasi.ac.in>
2024-10-13 20:30:06 +0530

    fixed delay

--- js/main.js
+++ js/main.js
@@ -51,17 +51,17 @@ sr.reveal('.steps__step', { distance: '100px', interval: 100});
 
 //about
 sr.reveal('.about__text', { origin: 'left' });
-sr.reveal('.about__img', { origin: 'right', delay: 800 });
+sr.reveal('.about__img', { origin: 'right', delay: 50 });
 //testimonial
-sr.reveal('.testimonial__bg', { delay: 800 });
+sr.reveal('.testimonial__bg', { delay: 50 });
 sr.reveal('.testimonial__title');
-sr.reveal('.testimonial__slider', { delay: 1000 });
+sr.reveal('.testimonial__slider', { delay: 50 });
 //brands
-sr.reveal('.brands__img', { delay: 600, distance: '100px', interval: 100 });
+sr.reveal('.brands__img', { delay: 50, distance: '100px', interval: 100 });
 //work
 sr.reveal('.work__title');
-sr.reveal('.work__subtitle', { delay: 800});
-sr.reveal('.work__grid', { delay: 1000});
+sr.reveal('.work__subtitle', { delay: 50});
+sr.reveal('.work__grid', { delay: 50});
 
 //stats
 sr.reveal('.stats');
@@ -72,17 +72,17 @@ sr.reveal('.stats__item', {
 
 //news
 sr.reveal('.news__title');
-sr.reveal('.news__subtitle', { delay: 800 });
+sr.reveal('.news__subtitle', { delay: 50 });
 sr.reveal('.news__item', { 
     distance: '100px',
     interval: 100,
-    delay: 1000,
+    delay: 50,
 });
 
 
 //contact
 sr.reveal('.contact__container');
-sr.reveal('.contact__text', { delay: 800 });
+sr.reveal('.contact__text', { delay: 50 });
 
 //footer
 sr.reveal('.footer__item', {
